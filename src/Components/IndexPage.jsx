c30f93353543022788252e0ef88ff185ef01ae16
jayesh29patidar <61138490+jayesh29patidar@users.noreply.github.com>
2022-03-29 19:09:14 +0530

    Add files via upload

---
+++ src/Components/IndexPage.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+// import NoteAppService from '../Services/NoteAppService';
+// import icon from '../Images/icon';
+
+class IndexPage extends Component {
+
+
+    registerUser = (e) => {
+        e.preventDefault();
+        this.props.history.push('/add-user');
+    }
+    loginUser = (e) => {
+        e.preventDefault();
+        this.props.history.push('/login-user');
+    }
+
+
+    render() {
+        return (
+            <div className="background-main">
+
+
+                <div className='main-view' >
+                    <h1>
+                        Welcome to NotesApp <img id="register_logo" src={require('../Images/register.gif')} width="50px" />
+                    </h1>
+                    <div className="login-signup">
+                        <button className="btn btn-dark " onClick={this.registerUser} >Register</button>
+                        &#9135;&#9135;&#9135;&#9135; or  &#9135;&#9135;&#9135;&#9135;
+                        <button className="btn btn-dark " onClick={this.loginUser} >Login </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default IndexPage;
